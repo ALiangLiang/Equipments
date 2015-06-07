ba8394cf2a016d16a40c3cc2de5cb3cf1b767f80
PeterKuKu <iles_90138@yahoo.com.tw>
2015-06-07 15:01:33 +0800

    你名字沒打 當然不能commit...

---
+++ Rear/club.sql
@@ -0,0 +1,32 @@
+-- phpMyAdmin SQL Dump
+-- version 2.10.2
+-- http://www.phpmyadmin.net
+-- 
+-- 主機: localhost
+-- 建立日期: Jun 07, 2015, 06:40 AM
+-- 伺服器版本: 5.0.45
+-- PHP 版本: 5.2.3
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+-- 
+-- 資料庫: `test`
+-- 
+
+-- --------------------------------------------------------
+
+-- 
+-- 資料表格式： `club`
+-- 
+
+CREATE TABLE `club` (
+  `id` int(20) NOT NULL,
+  `name` char(20) NOT NULL,
+  `acc num` char(20) NOT NULL,
+  `password` char(20) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- 
+-- 列出以下資料庫的數據： `club`
+-- 
+
